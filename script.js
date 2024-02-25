a6d5e7d1ce882e3998e2ba8c08da222365219e96
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-25 17:32:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+let input1=document.getElementById('fname');
+let input2=document.getElementById('lname');
+let input3=document.getElementById('no');
+let input4=document.getElementById('email');
+let button=document.getElementById('btn');
+
+function myFunction(){
+	alert(
+        "First Name: " + input1.value + "  " +
+        "Last Name: " + input2.value + "  " +
+        "Phone Number: " + input3.value + "  " +
+        "Email ID: " + input4.value
+    );
+}
